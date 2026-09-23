6abf187aff01fde5cac67def2bfc12d3e7063edb
Diego Avila <avilardiegoa@gmail.com>
2026-09-23 17:40:04 -0500

    fix: declare Neon Function using supported config shape

--- neon.ts
+++ neon.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from "@neon/config/v1";
 
 export default defineConfig({
-  preview: {
-    functions: {
-      zuara: {
-        name: "ZUARA API",
-        source: "./functions/zuara-api.ts",
-      },
+  functions: {
+    zuara: {
+      name: "ZUARA API",
+      source: "./functions/zuara-api.ts",
     },
   },
 });
